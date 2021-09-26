f54c7becda0b7931da8ef221eecf1b1c052e0b8e
James Routley <jroutley@gmail.com>
2021-09-26 15:58:40 +0100

    Updates

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"hash/fnv"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -69,8 +72,8 @@ func buildBook(config *Config) error {
 		return err
 	}
 
-	for _, chapter := range chapters {
-		if err := buildChapter(config, chapter); err != nil {
+	for i, chapter := range chapters {
+		if err := buildChapter(i+1, config, chapter); err != nil {
 			return err
 		}
 	}
@@ -78,7 +81,7 @@ func buildBook(config *Config) error {
 	return nil
 }
 
-func buildChapter(config *Config, chapter string) error {
+func buildChapter(number int, config *Config, chapter string) error {
 	contents, err := ioutil.ReadFile(chapter)
 	if err != nil {
 		return err
@@ -89,19 +92,36 @@ func buildChapter(config *Config, chapter string) error {
 		return err
 	}
 
-	filename := filepath.Join(buildDir, chapter+".html")
+	filename := fmt.Sprintf("%02d-%s.html", number, chapterHash("Great Expectations", number))
 
-	if err := os.MkdirAll(filepath.Dir(filename), 0755); err != nil {
+	path := filepath.Join(buildDir, "great-expectations", filename)
+
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
 		return err
 	}
 
-	variables := map[string]interface{}{
-		"Title":   "Great Expectations",
-		"Chapter": template.HTML(buf.String()),
-		"Styles":  template.CSS(styles),
+	type previousChapter struct {
+		Name string
+		URL  string
 	}
 
-	file, err := os.Create(filename)
+	var previousChapters []*previousChapter
+	for i := 1; i < number; i++ {
+		chapterName := fmt.Sprintf("%02d-%s.html", i, chapterHash("Great Expectations", i))
+		previousChapters = append(previousChapters, &previousChapter{
+			Name: fmt.Sprintf("Chapter %d", i),
+			URL:  chapterName,
+		})
+	}
+
+	variables := map[string]interface{}{
+		"Title":            "Great Expectations",
+		"Chapter":          template.HTML(buf.String()),
+		"Styles":           template.CSS(styles),
+		"PreviousChapters": previousChapters,
+	}
+
+	file, err := os.Create(path)
 	if err != nil {
 		return err
 	}
@@ -112,4 +132,14 @@ func buildChapter(config *Config, chapter string) error {
 	}
 
 	return nil
+}
+
+func chapterHash(bookName string, chapterNumber int) string {
+	return hash(fmt.Sprintf("reserialised-%s-%d", bookName, chapterNumber))
+}
+
+func hash(s string) string {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return base64.RawStdEncoding.EncodeToString(h.Sum(nil))
 }
